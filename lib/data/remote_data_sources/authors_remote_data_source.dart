282f99fd1ecc26730ef4668a4c33932cebf46f20
Marcin Eberst <marcin@v-zone.pl>
2023-01-29 19:30:04 +0100

    final commit

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,32 +1,28 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:user_articles/domain/models/author_model.dart';
+part 'authors_remote_data_source.g.dart';
 
-class AuthorsMockedDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    return [
-      {
-        'id': 1,
-        'picture': 'https://randomuser.me/api/portraits/men/57.jpg',
-        'first_name': 'Piotr',
-        'last_name': 'Obdarowicz',
-      },
-      {
-        'id': 2,
-        'picture': 'https://randomuser.me/api/portraits/men/58.jpg',
-        'first_name': 'Radosław',
-        'last_name': 'Gdański',
-      },
-    ];
-  }
+@injectable
+@RestApi()
+abstract class AuthorsRemoteRetrofitDataSource {
+  @factoryMethod
+  factory AuthorsRemoteRetrofitDataSource(Dio dio) =
+      _AuthorsRemoteRetrofitDataSource;
+
+  @GET("/users")
+  Future<List<AuthorModel>> getAuthors();
 }
 
-class AuthorsRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/users');
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
-}
+// class AuthorsRemoteDioDataSource {
+//   Future<List<Map<String, dynamic>>?> getAuthors() async {
+//     final response = await Dio().get<List<dynamic>>(
+//         'https://my-json-server.typicode.com/adamsmaka/json-demo/users');
+//     final listDynamic = response.data;
+//     if (listDynamic == null) {
+//       return null;
+//     }
+//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+//   }
+// }
--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -1,23 +1,16 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'author_model.freezed.dart';
 part 'author_model.g.dart';
 
-@JsonSerializable()
-class AuthorModel {
-  const AuthorModel({
-    required this.id,
-    required this.picture,
-    required this.firstName,
-    required this.lastName,
-  });
-
-  @JsonKey(name: 'id')
-  final int id;
-  @JsonKey(name: 'picture')
-  final String picture;
-  @JsonKey(name: 'first_name')
-  final String firstName;
-  @JsonKey(name: 'last_name')
-  final String lastName;
+@freezed
+class AuthorModel with _$AuthorModel {
+  const AuthorModel._();
+  factory AuthorModel(
+    int id,
+    String picture,
+    @JsonKey(name: 'first_name') String firstName,
+    @JsonKey(name: 'last_name') String lastName,
+  ) = _AuthorModel;
 
   String get name {
     return '$firstName $lastName';
@@ -25,5 +18,34 @@ class AuthorModel {
 
   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
       _$AuthorModelFromJson(json);
-  Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
 }
+
+//   @JsonKey(name: 'first_name') //jsonkey dodajemy tylko dla zmiennych, które mają inną nazwę zmiennej niż pole w api
+
+
+// @JsonSerializable()
+// class AuthorModel {
+//   const AuthorModel({
+//     required this.id,
+//     required this.picture,
+//     required this.firstName,
+//     required this.lastName,
+//   });
+
+//   @JsonKey(name: 'id')
+//   final int id;
+//   @JsonKey(name: 'picture')
+//   final String picture;
+//   @JsonKey(name: 'first_name')
+//   final String firstName;
+//   @JsonKey(name: 'last_name')
+//   final String lastName;
+
+//   String get name {
+//     return '$firstName $lastName';
+//   }
+
+//   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
+//       _$AuthorModelFromJson(json);
+//   Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
+// }
